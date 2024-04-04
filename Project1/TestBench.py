81200bf6706b97e8539cbf9e29ec335c7e5b5a81
Nicholas Chorette <nicpizza2000@gmail.com>
2024-04-03 22:32:29 -0500

    Appears that I THINK for .vhd files i cannot open text files, but it appears that for them, I can use the same methods I used last semester, which is for those test bench files, just set what you expect the output to be for that checking line, NOT what it should NOT be, but what IT SHOULD be. if it's NOT, then it will flag an error and not let the program proceed

--- Project1/TestBench.py
+++
@@ -1,9 +0,0 @@
-'''
-Author: 
-Date:
- Purpose: to automatically emulate me typing in information for inputs and outputs into the consol log/terminal. In theory, this SHOULD allow me to just edit the text file, run this bastard, and go from there. maybe not 
-
-'''
-
-f = open("demofile.txt", "r")
-print(f.readline())
